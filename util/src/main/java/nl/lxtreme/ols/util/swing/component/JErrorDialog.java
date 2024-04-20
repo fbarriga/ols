3969591e8e3e109a39ffb3ce2032d4dfbdb7bcec
Felipe Barriga Richards <felipe@felipebarriga.cl>
2024-04-19 22:58:59 -0400

    util: JErrorDialog: show exception and exception message

--- util/src/main/java/nl/lxtreme/ols/util/swing/component/JErrorDialog.java
+++ util/src/main/java/nl/lxtreme/ols/util/swing/component/JErrorDialog.java
@@ -401,6 +401,8 @@ public class JErrorDialog extends JDialog implements Closeable
       final Window owner = SwingComponentUtils.getCurrentWindow();
       final IncidentInfo incident = new IncidentInfo( "Uncaught exception...", //
           "<html><b>Something unexpected happened!</b><br><br>"
+              + "<b>Exception</b><br><pre>" + aException.getClass().getName() + "</pre><br>"
+              + "<b>Exception Message</b><br><tt>" + aException.getMessage() + "</tt><br><br>"
               + "Click on \"more details\" for more information about the possible cause.<br><br>"
               + "If the problem persists, please report it as bug.</html>", "", aException );
 
